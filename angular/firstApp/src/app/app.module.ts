0b89df8ecf580aa1c15f7ed3788e734d2f3c5726
Bruno Otávio <brunosantosprotec@gmail.com>
2019-05-03 15:21:58 -0300

    created the aplication components

--- angular/firstApp/src/app/app.module.ts
+++ angular/firstApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { PanelComponent } from './panel/panel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressComponent } from './progress/progress.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //novo component importado
+    TopComponent,
+    PanelComponent,
+    TentativasComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/firstApp/src/app/top/top.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+//selector: onde a view do component vai ser aplicada
+//templateUrl: onde está localizada o arquivo html da view 
+
+@Component({
+	selector: 'app-top',
+	templateUrl: './top.component.html',
+	styleUrls: ['./top.component.css']
+})
+
+export class TopComponent {
+	
+}
