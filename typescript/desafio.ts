8d76aa7b8e6e56d7ae353988465955fb74d4423f
Bruno Santos <bruno.santos@kukac.com.br>
2019-05-29 11:15:20 -0300

    challenge completed: convert a js code to ts code

---
+++ typescript/desafio.ts
@@ -0,0 +1,43 @@
+//converter para TS
+/*let contaBancaria = {
+    saldo: 3456,
+    depositar(valor){
+        this.saldo += valor
+    }
+}
+
+let correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890','98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)*/
+
+//codigo em TS
+
+type contaBancaria = {
+    saldo : number,
+    depositar(valor : number) : void 
+}
+
+type correntista = {
+    nome : string,
+    contaBancaria : contaBancaria,
+    contatos : string[]
+}
+
+let cliente : correntista = {
+    nome : 'Ana Silva',
+    contaBancaria : {
+        saldo : 3456,
+        depositar(valor : number) : void {
+            this.saldo += valor
+        }
+    },
+    contatos: ['34567890','98765432']
+}
+
+cliente.contaBancaria.depositar(3000)
+console.log(cliente)
